6f94c406f844e17eec08ae9935123448120392cf
Brian Tarbox <briantarbox@gmail.com>
2021-08-07 10:31:01 -0400

    Create linechartdata.py

---
+++ linechartdata.py
@@ -0,0 +1,24 @@
+linedata = {
+  "visualObjectData": {
+    "lineChartData": {
+      "primaryTickList": {
+        "ticks": [],
+        "tickMaxWidth": 3.5,
+        "startTickIndex": 0
+      },
+      "defaultValueTickList": {
+        "ticks": [],
+        "tickMaxWidth": 3.0,
+        "startTickIndex": 0
+      },
+
+      "maxValueTickWidth": 3.0,
+      "paths": [],
+      "calloutType": "hover"
+    }
+  },
+  "metadata": {
+    "backgroundImage": "",
+    "title": "Premier League Points By Match Week"
+  }
+}
